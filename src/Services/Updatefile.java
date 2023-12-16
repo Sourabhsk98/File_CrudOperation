541cf78b5c3962e6a4c5304a261e208ce54f4700
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2023-12-16 13:23:27 +0530

    Updatefile.java

--- src/Services/Updatefile.java
+++ src/Services/Updatefile.java
@@ -1,25 +1,61 @@
 package Services;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Updatefile {
-    public void update()  throws IOException {
 
-        Path myPath = Paths.get("D:\\PK\\Java\\Sourabh.txt");
+    public void update() throws IOException {
+        Scanner scanner = new Scanner(System.in);
 
-        List<String> lines = new ArrayList<>();
-        lines.add("Java is high level programming language!!");
+        // Get the file path from the user
+        
+        System.out.print("Enter the file path: ");
+        String filePath = scanner.nextLine();
+        Path myPath = Paths.get(filePath);
 
+        // Read the existing content of the file
+        
+        List<String> lines = Files.readAllLines(myPath, StandardCharsets.UTF_8);
 
+        // Display the existing content to the user
+        
+        System.out.println("Current content of the file:");
+        for (String line : lines) {
+            System.out.println(line);
+        }
+
+        // Get the line number to edit
+        
+        System.out.print("Enter the line number to edit: ");
+        int lineNumber = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        // Get the new content from the user
+        
+        System.out.print("Enter the new content: ");
+        String newContent = scanner.nextLine();
+
+        // Update the specified line with the new content
+        
+        if (lineNumber >= 1 && lineNumber <= lines.size()) {
+            lines.set(lineNumber - 1, newContent);
+        } else {
+            System.out.println("Invalid line number. No changes made.");
+            return;
+        }
+
+        // Write the updated content back to the file
+        
         Files.write(myPath, lines, StandardCharsets.UTF_8,
-                StandardOpenOption.CREATE);
+                StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
 
-        System.out.println("Data written to a file");
+        System.out.println("File content updated successfully.");
     }
 }
